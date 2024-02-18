6110e18def05f241b4603a2bbc964236b86c3586
sotetu79 <sotetu79@gmail.com>
2024-02-18 20:02:53 +0900

    add sending method to send_email.py

--- send_email.py
+++ send_email.py
@@ -5,3 +5,10 @@ with open("/home/pi/.local/share/.email_password", "r") as f:
     password = f.read()
 
 yag = yagmail.SMTP("kousoubirusangou@gmail.com", password)
+
+yag.send(
+    to="kenta.sato.05@gmail.com",
+    subject="first email",
+    contents="Hello from Raspberry Pi",
+)
+print("Email sent successfully")
