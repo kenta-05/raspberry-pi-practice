bb6793732206f766247dd4b1fe483c1864aa3007
sotetu79 <sotetu79@gmail.com>
2024-02-19 18:29:32 +0900

    add /push-button

--- web_server.py
+++ web_server.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import RPi.GPIO as GPIO
 
+BUTTON_PIN = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
 app = Flask(__name__)
 
 
@@ -8,4 +13,12 @@ def index():
     return "Hello, World!"
 
 
-app.run(host="0.0.0.0")
+@app.route("/push-button")
+def is_button_pushed():
+    if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+        return "Button is pushed"
+    else:
+        return "Button is not pushed"
+
+
+app.run(host="0.0.0.0", port=8500)
