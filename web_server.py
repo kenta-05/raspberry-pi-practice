55317a826c1dd1f1a770e16c917f0e688a6c1fcc
sotetu79 <sotetu79@gmail.com>
2024-02-23 20:03:32 +0900

    add feature related to led in web_server.py

--- web_server.py
+++ web_server.py
@@ -2,10 +2,14 @@ from flask import Flask
 import RPi.GPIO as GPIO
 
 BUTTON_PIN = 26
+LED_PIN_LIST = [17, 27, 22]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN)
 app = Flask(__name__)
+
+for led_pin in LED_PIN_LIST:
+    GPIO.setup(led_pin, GPIO.OUT)
 
 
 @app.route("/")
@@ -19,6 +23,19 @@ def is_button_pushed():
         return "Button is pushed"
     else:
         return "Button is not pushed"
+
+
+@app.route("/led/<int:led_pin>/state/<int:led_state>")
+def trigger_led(led_pin, led_state):
+    if led_pin in LED_PIN_LIST:
+
+        for pin in LED_PIN_LIST:
+            GPIO.output(pin, GPIO.LOW)
+
+        GPIO.output(led_pin, led_state)
+        return f"LED {led_pin} is now {led_state}"
+    else:
+        return f"LED {led_pin} is not available"
 
 
 app.run(host="0.0.0.0", port=8500)
